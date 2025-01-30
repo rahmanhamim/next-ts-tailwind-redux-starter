722ac26dbe00c35349def7df748a1d2d576346ad
rahmanhamim <rahmanhamiminfo@gmail.com>
2025-01-30 23:48:07 +0600

    updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import js from "@eslint/js"
-import next from "@next/eslint-plugin-next"
+import js from '@eslint/js'
+import next from '@next/eslint-plugin-next'
 
 export default [
   js.configs.recommended,
   next.configs.recommended,
   {
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "warn",
-      "no-undef": "warn",
-      quotes: ["error", "single"],
-      "react/display-name": "off",
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'no-undef': 'warn',
+      quotes: ['error', 'single'],
+      'react/display-name': 'off',
     },
   },
 ]
